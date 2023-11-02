1c35805edeb6bf9078117e3de52feac7ef84842c
Bruno Alvisio <bruno.alvisio@gmail.com>
2023-11-02 11:33:32 +0000

    Add comment about sources

--- qa.py
+++ qa.py
@@ -39,4 +39,19 @@ chain = RetrievalQAWithSourcesChain.from_chain_type(
 )
 result = chain({"question": "How do I request vacation??"})
 print(f"Answer: {result['answer']}")
+
+# The output of the chain has three keys:
+# - answer
+# - sources
+# - source_documents
+#
+# These are defined as the `output_keys` property of the `BaseQAWithSourcesChain`:
+# https://github.com/langchain-ai/langchain/blob/8bd3ce59cd4ef80db5eb52922bb31588596939e1/libs/langchain/langchain/chains/qa_with_sources/base.py#L104
+
+# Noticed that the sources are always empty but the source_documents are not.
+# The following github issues talk about this:
+# - https://github.com/langchain-ai/langchain/issues/5642
+# - https://github.com/langchain-ai/langchain/issues/11387
+# It is solved by this PR:
+# - https://github.com/langchain-ai/langchain/pull/12556
 print(f"Sources: {result['sources']}")
